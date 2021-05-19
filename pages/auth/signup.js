1039ccd2f4df616d6656d3499b3374cb741bc120
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-19 15:17:34 +0100

    handling validation errors in signup pages

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -4,15 +4,22 @@ import axios from 'axios';
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await axios.post('/api/users/signup', {
-      email, password
-    });
+    try {
+      
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      });
 
-    console.log(response.data);
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+
   }
 
   return (
@@ -26,6 +33,12 @@ export default () => {
         <label>Password</label>
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control" />
       </div>
+      {errors.length > 0 && <div className="alert alert-danger">
+        <h4>Ooooops...</h4>
+        <ul className="my-0">
+          {errors.map(err => <li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
