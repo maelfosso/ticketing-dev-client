86b6c54e4b9cc1f80bc9e354f73076f27de064e8
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-21 20:27:17 +0100

    different base url for client or server environment

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log('into the component', currentUser);
-  // axios.get('/api/users/currentuser');
-
-
+  console.log('CURRENT USER', currentUser);
   return <h1>Landing page</h1>;
 }
 
@@ -12,9 +9,23 @@ LandingPage.getInitialProps = async () => {
   if (typeof window === 'undefined') {
     // we are on the server!
     // request should be made to http://ingress-nginx.i
+    const { data } = await axios.get(
+      'http://ingress-nginx.kube-system.svc.cluster.local/api/users/currentuser', {
+        'headers': {
+          Host: 'ticketing.dev'
+        }
+      }
+    )
+    console.log('WINDOWS UNDEFINED');
+
+
+    return data;
   } else {
     // we are on the browser
     // request can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
   }
   return {};
 }
