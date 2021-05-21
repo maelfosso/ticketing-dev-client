53bd34d4fa82380d8fe7699fbe41cd859066e4cc
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-21 22:03:14 +0100

    calling getInitialProps of another page from AppComponent

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ const LandingPage = ({ currentUser }) => {
 
 LandingPage.getInitialProps = async ({ req }) => {
   console.log('Landing Page');
+  
   const client = buildClient({ req });
   const { data } = await client.get('/api/users/currentuser');
   return data;
