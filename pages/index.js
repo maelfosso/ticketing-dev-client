ea4cf7a0ddd8d640c8424ea64208f853381e935e
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-21 21:30:44 +0100

    landing page content customized according to the current user value

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log('CURRENT USER', currentUser);
-  return <h1>Landing page</h1>;
+  return currentUser ? <h1>You are signed in </h1> : <h1>Landing page</h1>;
 }
 
 LandingPage.getInitialProps = async ({ req }) => {
