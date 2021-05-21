b4d8e553843032101317161bcc93ee1834ec893b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-21 22:12:23 +0100

    added Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
-  return <div>
-    <h1>Header! { currentUser && currentUser.email }</h1>
-    <Component {...pageProps} />
+  return (
+    <div>
+      <Header currentUser={currentUser}></Header>
+      <Component {...pageProps} />
     </div>
+  );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
