6cce221e1646f73d1321804b2d665205a105543e
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-21 17:03:51 +0100

    determine in which side we are, client or server

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component ', color);
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log('into the component', currentUser);
+  // axios.get('/api/users/currentuser');
+
 
   return <h1>Landing page</h1>;
 }
 
-LandingPage.getInitialProps = () => {
-  console.log('I am on a the server');
-
-  return { color: 'red' };
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // request should be made to http://ingress-nginx.i
+  } else {
+    // we are on the browser
+    // request can be made with a base url of ''
+  }
+  return {};
 }
 
 export default LandingPage;
