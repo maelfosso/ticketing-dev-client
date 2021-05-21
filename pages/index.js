de17cee722ac0ca3d63a205bfefb393471d44c64
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-21 15:58:53 +0100

    example of usage of getComponentProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+const LandingPage = ({ color }) => {
+  console.log('I am in the component ', color);
+
   return <h1>Landing page</h1>;
 }
+
+LandingPage.getInitialProps = () => {
+  console.log('I am on a the server');
+
+  return { color: 'red' };
+}
+
+export default LandingPage;
